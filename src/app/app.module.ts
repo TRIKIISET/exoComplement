d2d079fab4bc46269d8ce26d1f7dd4bdb0415448
AMTRIKI <amel.triki@gmail.com>
2019-11-07 10:11:45 +0100

    Création projet et 2 composants

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { PersonneComponent } from './partie1/personne/personne.component';
+import { PersonneListComponent } from './partie1/personne-list/personne-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PersonneComponent,
+    PersonneListComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/partie1/personne-list/personne-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-personne-list',
+  templateUrl: './personne-list.component.html',
+  styleUrls: ['./personne-list.component.css']
+})
+export class PersonneListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
